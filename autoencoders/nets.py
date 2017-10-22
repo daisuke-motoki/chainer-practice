b255e830c2b96d02abf8f42ee76bf113048a2035
Daisuke Motoki <daisuke.motoki@p8fbd57af.kngwnt01.ap.so-net.ne.jp>
2017-10-22 13:47:02 +0900

    implement cae

--- autoencoders/nets.py
+++ autoencoders/nets.py
@@ -5,31 +5,9 @@ import chainer.functions as F
 import chainer.links as L
 
 
-class AutoEncoder(Chain):
+class EncoderDecoder(object):
     """
     """
-    def __init__(self, input_dim, n_units):
-        """ init
-        Args:
-            n_units: list, int: List of the number of units.
-        """
-        super(AutoEncoder, self).__init__()
-        self.input_dim = input_dim
-        self.n_units = n_units
-        self.n_link = len(n_units)
-
-        W = initializers.HeNormal()
-        self.add_link("encode_0",
-                      L.Linear(input_dim, n_units[0], initialW=W))
-        for i in range(1, self.n_link, 1):
-            self.add_link("encode_{}".format(i),
-                          L.Linear(n_units[i-1], n_units[i], initialW=W))
-        for i in range(self.n_link-1, 0, -1):
-            self.add_link("decode_{}".format(i),
-                          L.Linear(n_units[i], n_units[i-1], initialW=W))
-        self.add_link("decode_0",
-                      L.Linear(n_units[0], input_dim, initialW=W))
-
     def __call__(self, x):
         """
         """
@@ -60,6 +38,33 @@ class AutoEncoder(Chain):
         report({"loss": loss_value}, self)
         return loss_value
 
+
+class AutoEncoder(Chain, EncoderDecoder):
+    """
+    """
+    def __init__(self, input_dim, n_units):
+        """ init
+        Args:
+            input_dim: int
+            n_units: list, int: List of the number of units.
+        """
+        super(AutoEncoder, self).__init__()
+        self.input_dim = input_dim
+        self.n_units = n_units
+        self.n_link = len(n_units)
+
+        W = initializers.HeNormal()
+        self.add_link("encode_0",
+                      L.Linear(input_dim, n_units[0], initialW=W))
+        for i in range(1, self.n_link, 1):
+            self.add_link("encode_{}".format(i),
+                          L.Linear(n_units[i-1], n_units[i], initialW=W))
+        for i in range(self.n_link-1, 0, -1):
+            self.add_link("decode_{}".format(i),
+                          L.Linear(n_units[i], n_units[i-1], initialW=W))
+        self.add_link("decode_0",
+                      L.Linear(n_units[0], input_dim, initialW=W))
+
     def save_structure(self, filename):
         """
         """
@@ -68,6 +73,57 @@ class AutoEncoder(Chain):
             dict(
                 input_dim=self.input_dim,
                 n_units=self.n_units
+            ),
+            open(filename, "w"),
+            indent=4
+        )
+
+
+class ConvolutionalAutoEncoder(Chain, EncoderDecoder):
+    """
+    """
+    def __init__(self, input_channel, n_channels):
+        """ init
+        Args:
+            n_channels: list, int: List of the number of channels.
+        """
+        super(ConvolutionalAutoEncoder, self).__init__()
+        self.input_channel = input_channel
+        self.n_channels = n_channels
+        self.n_link = len(n_channels)
+
+        W = initializers.HeNormal()
+        self.add_link(
+            "encode_0",
+            L.Convolution2D(input_channel, n_channels[0],
+                            4, 2, 1, initialW=W)
+        )
+        for i in range(1, self.n_link, 1):
+            self.add_link(
+                "encode_{}".format(i),
+                L.Convolution2D(n_channels[i-1], n_channels[i],
+                                4, 2, 1, initialW=W)
+            )
+        for i in range(self.n_link-1, 0, -1):
+            self.add_link(
+                "decode_{}".format(i),
+                L.Deconvolution2D(n_channels[i], n_channels[i-1],
+                                  4, 2, 1, initialW=W)
+            )
+        self.add_link(
+            "decode_0",
+            L.Deconvolution2D(n_channels[0], input_channel,
+                              4, 2, 1, initialW=W)
+        )
+
+    def save_structure(self, filename):
+        """
+        """
+        import json
+        json.dump(
+            dict(
+                input_channel=self.input_channel,
+                n_channels=self.n_channels
             ),
             open(filename, "w"),
             indent=4
